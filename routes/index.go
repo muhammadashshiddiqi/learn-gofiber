3548ff1595a94981993b61e28381fa01599fc538
Muhammad Ashshiddiqi <ashshiddiqi94@gmail.com>
2024-01-21 23:52:57 +0700

    update static assets

--- routes/index.go
+++ routes/index.go
@@ -1,12 +1,16 @@
 package routes
 
 import (
+	"crud-fiber/config"
 	"crud-fiber/controllers"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func InitRoutes(r *fiber.App) {
+	r.Static("/images", config.RootPath+"/public/assets/images")
+	r.Static("/files", config.RootPath+"/public/assets/files")
+
 	r.Get("/users", controllers.GetUserAll)
 	r.Post("/user", controllers.CreateUser)
 	r.Get("/user/:id", controllers.UserGetById)
